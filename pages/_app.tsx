0df4c4b9f2c3c036884825e59bf366b76acce605
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-04 17:33:34 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <Analytics />     
+      <GoogleAnalytics />
+      
     </>
   );
 }
