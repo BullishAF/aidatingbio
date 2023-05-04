ec64df23a6726e8da1103b2892f7f60900049a06
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-04 09:35:22 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,23 +6,11 @@ export default function Header() {
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
       
-        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
+        <h1 className="sm:text-4xl text-2xl text-yellow-300 font-bold ml-2 tracking-tight">
        🔥 tinderbio.com
         </h1>
       </Link>
-      <a
-        href="https://vercel.com/templates/next.js/twitter-bio"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          alt="Vercel Icon"
-          src="/vercelLogo.png"
-          className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-          width={32}
-          height={28}
-        />
-      </a>
+
     </header>
   );
 }
