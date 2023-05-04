cd2415adaccef7794d512a7e53e78c6877e1b2e0
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-04 13:25:38 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,10 +14,10 @@ export default function Footer() {
          © ChatGPT{" "}
         </a>
       </div>
-      <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
+      <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
     © 2023 Copyright:
     <a
-      class="text-neutral-800 dark:text-neutral-400"
+      className="text-neutral-800 dark:text-neutral-400"
       href="https://github.com/BullishAF/datingbio"
       >Ryan Parker</a
     >
