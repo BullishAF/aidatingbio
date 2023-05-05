ea07bf798f25b960c124f8d2758f0f365fb77ea3
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-05 12:30:07 -0400

    Update and rename GitHub.tsx to ProductHunt.tsx

--- components/GitHub.tsx
+++
@@ -1,5 +0,0 @@
-export default function ProductHunt({ className }: { className?: string }) {
-  return (
-   <a href="https://www.producthunt.com/posts/ai-dating-bio-generator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-ai&#0045;dating&#0045;bio&#0045;generator" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=392812&theme=light" alt="AI&#0032;Dating&#0032;Bio&#0032;Generator Generate&#0032;your&#0032;dating&#0032;app&#0032;bio&#0032;in&#0032;seconds&#0032;using&#0032;AI Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
-  );
-}
