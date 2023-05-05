6e3af15297254bbeb7e83760ee65756fbe886366
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-05 11:54:01 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,19 @@ export default function Header() {
        💞 AI Dating Bio Generator
         </h1>
       </Link>
-<a href="https://www.producthunt.com/posts/ai-dating-bio-generator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-ai&#0045;dating&#0045;bio&#0045;generator" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=392812&theme=light" alt="AI&#0032;Dating&#0032;Bio&#0032;Generator - Generate&#0032;your&#0032;dating&#0032;app&#0032;bio&#0032;in&#0032;seconds&#0032;using&#0032;AI | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+<a href="https://www.producthunt.com/posts/ai-dating-bio-generator" 
+   target="_blank"
+   rel="noreferrer"
+   >
+  <image 
+    src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=392812&theme=light" 
+    alt="AI Dating Bio Generator" 
+    className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
+    width={200}
+    height={50}
+    />         
+         
+ </a>
 </header>
   );
 }
