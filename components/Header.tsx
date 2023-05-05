6cf80847dd9f381099a3d012e37edf995b30efde
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-05 13:43:21 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
         <Image
           alt="Product Hunt"
           src="/featured.png"
-          className="sm:w-8 sm:h-[100px] w-8 h-[100px]"
+          className="sm:w-96 sm:h-[100px] w-96 h-[100px]"
           width={250}
           height={54}
         />
