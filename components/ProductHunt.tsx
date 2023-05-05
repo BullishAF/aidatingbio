36d99db858f5098b18a09647904fb1db0e9ec375
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-05 12:31:44 -0400

    Update ProductHunt.tsx

--- components/ProductHunt.tsx
+++ components/ProductHunt.tsx
@@ -5,8 +5,8 @@ export default function ProductHunt({ className }: { className?: string }) {
       width="50"
       height="50"
       fill="#ec407a"
-     viewBox="0 0 100 100">
-     className={className}
+      viewBox="0 0 100 100"
+      className={className}
     >
       <path d="M326.3 218.8c0 20.5-16.7 37.2-37.2 37.2h-70.3v-74.4h70.3c20.5 0 37.2 16.7 37.2 37.2zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-128.1-37.2c0-47.9-38.9-86.8-86.8-86.8H169.2v248h49.6v-74.4h70.3c47.9 0 86.8-38.9 86.8-86.8z"/>
  </svg>
